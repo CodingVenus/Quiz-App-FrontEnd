df011565666274782c84895ad3ee276d12316018
Venus Teah <venus.teah@gmail.com>
2022-02-25 12:57:32 -0500

    Added quiz, results and navbar components

--- quiz-app/src/app/app.module.ts
+++ quiz-app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CategoryService } from './category.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
